6d5a062bf7d8fe6037b5ba1bb7b49c2f1255deba
Vasanth Mohan <vmohan7@stanford.edu>
2016-01-19 10:20:16 -0800

    Add README

---
+++ GearVRTutorial/Assets/TransitionManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransitionManager : MonoBehaviour {
+
+	public static TransitionManager Instance;
+
+	private int currentScene = 0;
+
+	void Awake() {
+		if (Instance == null)
+			Instance = this;
+	}
+
+	void OnDestroy() {
+		if (Instance == this)
+			Instance = null;
+	}
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			LoadNextScene ();
+		}
+	}
+
+	private void LoadNextScene() {
+		currentScene = (currentScene + 1) % Application.levelCount;
+		Application.LoadLevel (currentScene);
+	}
+}
